feb82e01993a3fc270d289e8921c6a4dfab32fe7
Vitor <vmacedo99@gmail.com>
2022-10-16 12:45:28 -0300

    Atualizando relatório - Falando sobre congergência

--- Atividade1/resolucao/Gibbs.R
+++ Atividade1/resolucao/Gibbs.R
@@ -1,18 +1,30 @@
 library(readr)
 library(tidyverse)
-dados <- read_table("D:/UNICAMP/Disciplinas/COMPUTACIONAL/Atividade1/resolucao/dados.txt", 
+dados <- read_table("./Atividade1/resolucao/dados.txt", 
                     col_names = FALSE)
-colnames(dados) <- c('Individuo',paste0('armadilha',1:14))
+colnames(dados) <- c('onca',paste0('armadilha',1:14))
 nj <- dados%>%
-  select(-Individuo)%>%
-  colSums()
-Ngibbs=50e4
-x0 <- c(rpois(1,50),runif(14))
-chain <- matrix(0,ncol = Ngibbs,nrow=length(x0))
-chain[,1] <- x0
+  pivot_longer(-onca,values_to = "capturada",
+               names_to = "armadilha",names_prefix = 'armadilha',
+               names_transform = list(armadilha = as.numeric))%>%
+  group_by(armadilha)%>%
+  summarise(nj=sum(capturada))
+mj <- dados%>%
+  pivot_longer(-onca,values_to = "capturada",names_to = "armadilha",
+               names_prefix = 'armadilha',
+               names_transform = list(armadilha = as.numeric))%>%
+  group_by(onca)%>%
+  mutate(mj=case_when(cumsum(capturada)*capturada>1 ~ 1 , TRUE ~ 0))%>%
+  group_by(armadilha)%>%
+  summarise(mj=sum(mj))
+Ngibbs=10e4
+iter0 <- c(rpois(1,50),runif(14))
+chain <- matrix(0,ncol = Ngibbs,nrow=length(iter0))
+chain[,1] <- iter0
 rownames(chain) <- c("N",paste0("p",1:14))
 lambda=50
-r <- 12
+r <- sum(nj$nj)-sum(mj$mj)
+nj <- nj$nj 
 for (i in 2:Ngibbs) {
   lambda_pos <- lambda * prod(1-chain[-1,i-1])
   N_i_plus_1 <- rpois(n=1,lambda = lambda_pos) + r
@@ -22,6 +34,5 @@ for (i in 2:Ngibbs) {
     chain[j+1,i] <- aux
   }
 }
-final_sample <- chain[,seq(Ngibbs/2,Ngibbs,by=10)]
-plot(final_sample[1,],type='l')
-rowMeans(final_sample)
+summary(coda::mcmc(t(chain)))
+
