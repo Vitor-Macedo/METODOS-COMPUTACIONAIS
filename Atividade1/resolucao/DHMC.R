a01b029e1c71b8b81d2ba1032b4e8a2d07ef7744
Vitor <vmacedo99@gmail.com>
2022-10-21 01:44:09 -0300

    Atualizando trabalho

--- Atividade1/resolucao/DHMC.R
+++ Atividade1/resolucao/DHMC.R
@@ -1,27 +1,37 @@
-library(tidyverse)
-setwd("D:/UNICAMP/Disciplinas/COMPUTACIONAL/Atividade1/resolucao")
-theme_set(theme_bw())
-lambda=50
-df <- data.frame(grid=25:75)%>%
-  mutate(poi=dpois(x = grid,lambda = lambda),
-         gaus=dnorm(x = grid,mean = lambda,sd = sqrt(lambda)))
-ggplot(df,aes(x=grid,y=poi))+
-  geom_bar(stat='identity',fill='#ff6666')+
-  geom_point(size=.1)+
-  labs(y='f(x)',x='x')+
-  geom_function(fun=dnorm,args=list(mean=lambda,sd=sqrt(lambda)),size=.1)
-ggsave(filename = './imgs/g1.pdf',width = 4,height = 2.5)
-df <- data.frame(q = ppoints(20))%>%
-  mutate(pois=qpois(q,lambda),
-         gauss=qnorm(q,lambda,sqrt(lambda)))
-ggplot(df,aes(y=pois,x=gauss))+
-  geom_point()+
-  labs(x='Gaussian quantiles',y='Poisson quantiles')+
-  geom_abline(slope = 1,intercept = 0,col='blue')
-ggsave(filename = './imgs/g2.pdf',width = 4,height = 2.5)
-
-mod <- rstan::stan_model('cap_recap.stan')
-nj <- c(2,4,2,4,4,2,3,4,2,3,5,10,2,5)
-mj <- c(0,0,2,4,4,2,3,2,2,3,5,6,2,5)
-MCMC <- rstan::sampling(object=mod,data=list(j=14,ni=nj,mi=mj),chains=4,iter=2000,control=list(max_treedepth=15))
-chains <- rstan::extract(MCMC)
+library(mvtnorm)
+U <- function(theta,r=12,ni){
+  N <- theta[1]
+  pi <- theta[2:length(theta)]
+  Ux <- log(gamma(N+1))-log(gamma(N-r+1)) + sum(ni*log(pi)+(N-ni)*log(1-pi)) - .01*(N-50)^2
+  return(-Ux)
+}
+K <- function(M,rho){
+  .5*t(rho)%*%M%*%rho
+}
+dUdt <- function(theta,r=12,ni){
+  N <- theta[1]
+  pi <- theta[2:length(theta)]
+  dUdp <- (N*p-ni)/(pi*(1-pi))
+  dUdN <- -(digamma(N+1)/gamma(N+1)) + (digamma(N-r+1)/gamma(N-r+1)) - sum(log(1-pi)) + .02*(N-50)
+  grad <- c(dUdN,dUdp)
+  return(grad)
+}
+dKdrho <- function(M,rho){
+  
+}
+iter=500
+sample <- matrix(0,nrow = iter,ncol=15)
+colnames(sample) <- c("N",paste0("p",1:14))
+sample[1,] <- c(rpois(1,50),runif(14))
+M <- cor(gbs%>%select(N,p1:p14))
+L=50;delta=.01
+for (i in 2:iter) {
+  rho0 <- theta0 <- matrix(0,nrow = L,ncol = 15)
+  rho0[1,] <- rmvnorm(1, mean = rep(0,15), sigma = M)
+  theta0[1,] <- sample[i-1,]
+  #leapfrog
+  for (j in 1:L) {
+    
+  }
+  
+}
--- Atividade1/resolucao/Gibbs.R
+++ Atividade1/resolucao/Gibbs.R
@@ -18,6 +18,7 @@ mj <- dados%>%
   mutate(mj=case_when(cumsum(capturada)*capturada>1 ~ 1 , TRUE ~ 0))%>%
   group_by(armadilha)%>%
   summarise(mj=sum(mj))
+nj <- nj$nj 
 Ngibbs=10e4
 iter0 <- c(rpois(1,50),runif(14))
 chain <- matrix(0,ncol = Ngibbs,nrow=length(iter0))
@@ -35,5 +36,65 @@ for (i in 2:Ngibbs) {
     chain[j+1,i] <- aux
   }
 }
-rowMeans(chain)
 
+gibbs_popSize <- function(iter=500,chains=4,r=12,nj,lambda=50){
+  sample <- NULL
+  aux <- matrix(0,nrow = iter,ncol=15)
+  colnames(aux) <- c("N",paste0("p",1:14))
+  for (k in 1:chains) {
+    iter0 <- c(rpois(1,50),runif(14))
+    aux[1,] <- iter0
+    for (i in 2:iter) {
+      lambda_pos <- lambda * prod(1-aux[i-1,-1])
+      N_i_plus_1 <- rpois(n=1,lambda = lambda_pos) + r
+      aux[i,1] <- N_i_plus_1 
+      for (j in 1:length(nj)) {
+        aux2 <- rbeta(n = 1,shape1 = nj[j]+1,shape2 = N_i_plus_1-nj[j]+1)
+        aux[i,j+1] <- aux2
+      }
+    }
+    sample <- bind_rows(sample,data.frame(aux,chain=as.character(k),iteration=1:iter))
+  }
+  return(sample)
+}
+
+gbs <- gibbs_popSize(iter = 1000,chains = 4,r = 12,nj = nj,lambda = 50)
+gbs%>%
+  ggplot(aes(x=iteration,y=N,color=chain))+
+  geom_line(alpha=.5)+
+  theme_bw()
+ggsave(filename = paste0('./imgs/N','.pdf'),width = 4,height = 2.5)
+
+for (i in 1:14) {
+  gbs%>%
+    select(-N)%>%
+    pivot_longer(cols = c(-iter,-chain),names_to = 'param',values_to = 'pi')%>%
+    filter(param==paste0('p',i))%>%
+    ggplot(aes(x=iter,y=pi,color=chain))+
+    geom_line(alpha=.5)+
+    labs(y="p")+
+    theme_bw()
+ggsave(filename = paste0('./imgs/p',i,'.pdf'),width = 4,height = 2.5)
+}
+
+# ACF N
+Nacf <- acf(gbs%>%filter(chain==1)%$%N)
+Nacfdf <- with(Nacf, data.frame(lag, acf))
+ggplot(data = Nacfdf, mapping = aes(x = lag, y = acf)) +
+  geom_hline(aes(yintercept = 0)) +
+  geom_segment(mapping = aes(xend = lag, yend = 0))+
+  geom_hline(yintercept = c(-.025,.025),linetype=2,col='blue')+
+  theme_bw()
+ggsave(filename = paste0('./imgs/Nacf','.pdf'),width = 4,height = 2.5)
+gbs%>%
+  select(p1:p14,N,iteration)%>%
+  pivot_longer(cols = -iteration,names_to = 'param',values_to = 'valor')%>%
+  mutate(param=factor(param,levels = c('N',paste0('p',1:14))))%>%
+  group_by(param)%>%
+  summarise(media=mean(valor),
+            mediana=median(valor),
+            lower=quantile(valor,.025)%>%round(3),
+            upper=quantile(valor,.975)%>%round(3))%>%
+  mutate(ic=paste0('[',lower,';',upper,']'))%>%
+  select(param,media,mediana,ic)%>%
+  xtable::xtable(digits = 3)
